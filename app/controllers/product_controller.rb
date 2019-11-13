ecbc456459031d46f4d8d567d24cd03ffd848e3e
gentur ariyadi <gentur.ariya@gmail.com>
2019-11-13 11:19:11 +0700

    add feature add product

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,92 @@
+class ProductController < ApplicationController
+    # skip_before_filter:verify_authenticity_token
+    # rescue_from ActiveRecord::RecordNotFound, with: :notFound    
+    
+    def create_product
+        @product = Product.new(product_params)
+        if @product.save
+            render json:{
+                values:@product,
+                message:"success!"
+            },status:200
+        else
+            render json:{
+                values:{},
+                message:"Failed!"
+            },status:200
+        end
+    end
+
+    def update_product
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            render json:{
+                values:{},
+                message:"success!"
+            },status:200
+        else
+            render json:{
+                values:{},
+                message:"Failed!"
+            },status:400
+            
+        end
+    end
+    
+
+    def show_all
+        @product = Product.all
+        
+        render json:{
+            values: @product,
+            message: "success!"
+        },status:200
+    end
+
+    def show_by_id
+        @product = Product.find(params[:id])
+        if @product.present?
+            render json:{
+                values:@product,
+                message: 'Success!'
+            }, status:200
+        else
+            render json:{
+                values:"",
+                message:"we can't found any data!"
+            }, status:400
+        end
+    end
+    
+    
+
+    def destroy_product
+        @product = Product.find(params[:id])
+        
+        if @product.destroy
+            render json:{
+                values:{},
+                message:"success!",
+            },status:200
+        end
+    end
+    
+
+    private
+    def product_params
+        params.require(:product).permit(:name,:description,:enable)       
+    end
+
+    def category_params
+        params.require(:category).permit(:name_category,:enable)        
+    end
+    
+
+    def notFound
+        render json:{
+            values:{},
+            message: "Data Not Found!",
+        }, status:400
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'products' => 'product#create_product'
+  get 'products' => 'product#show_all'
+  get 'products/:id' => 'product#show_by_id'
+  put 'products/:id' => 'product#update_product'
+  delete 'products/:id' => 'product#destroy_product'
 end
