f807ae9690a513c1e8d67fe7727bcc0ee09a323a
gentur ariyadi <gentur.ariya@gmail.com>
2019-11-14 00:30:00 +0700

    add feature add product with image id & show products with category and image

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -6,11 +6,13 @@ class ProductController < ApplicationController
         @product = Product.new(product_params)
         if @product.save
             @category_product = CategoryProduct.new(:product_id => @product.id, :category_id => params[:category_id])
-            if @category_product.save
+            @product_image = ProductImage.new(:product_id => @product.id, :image_id => params[:image_id])
+            if @category_product.save && @product_image.save
                 render json:{
                     values:{
                         product: @product,
-                        category_product: @category_product
+                        category_product: @category_product,
+                        image: @product_image
                     },
                     message:"success!"
                 },status:200 
@@ -34,14 +36,37 @@ class ProductController < ApplicationController
         if @product.update(product_params)
             
             @category_product = CategoryProduct.where({:product_id => @product.id}).update_all({:category_id => params[:category_id]})
-            if @category_product
-                render json:{
-                    values:{
-                        product: @product,
-                        category: @category_product
-                    },
-                    message:"success!"
-                },status:200
+            @product_image = ProductImage.where({:product_id => @product.id}).update_all({:image_id => params[:image_id]})
+            if @category_product && @product_image
+                if @product_image == 0
+                    @product_image = ProductImage.new(:product_id => @product.id, :image_id => params[:image_id])
+                    if @product_image.save
+                        render json:{
+                            values:{
+                                product: @product,
+                                category: @category_product,
+                                image: @product_image
+                            },
+                            message:"success!"
+                        },status:200
+                    else
+                        render json:{
+                            values:{},
+                            message:"Failed!"
+                        },status:400                        
+                    end
+
+                else
+                    render json:{
+                        values:{
+                            product: @product,
+                            category: @category_product,
+                            image: @product_image
+                        },
+                        message:"success!"
+                    },status:200
+                end
+                
             else
                 render json:{
                     values:{},
@@ -61,18 +86,82 @@ class ProductController < ApplicationController
     def show_all
         # @product = Product.all
         @product = Product.find_by_sql('SELECT * FROM products where enable =true')
+
+        @products = []
+        @product.each do |item|
+            @one_product = {}
+            @one_product[:id] = item[:id]
+            @one_product[:name] = item[:name]
+            @one_product[:description] = item[:description]
+
+            @product_image = ProductImage.where({:product_id => item['id']}).limit(1)
+            if @product_image.length != 0
+                @one_product[:image_id] = @product_image[0]['image_id']
+
+                @image = Image.find(@one_product[:image_id])
+                @one_product[:image] = @image.file.url                
+                
+            else
+                @one_product[:image_id] = '-'
+                @one_product[:image] = '-'
+                
+            end
+
+            @category_product = CategoryProduct.where({:product_id => item['id']}).limit(1)
+            if @category_product.length != 0
+                @one_product[:category_id] = @category_product[0]['category_id']
+
+                @category = Category.find(@one_product[:category_id])
+
+                @one_product[:category] = @category['name']
+                @products.push(@one_product)
+            else
+                @one_product[:category_id] = '-'
+                @one_product[:category] = '-'
+
+                @products.push(@one_product)                 
+            end
+
+
+        end
         
         render json:{
-            values: @product,
+            values: @products,
             message: "success!"
         },status:200
     end
 
     def show_by_id
-        @product = Product.find(params[:id])
+        @product = Product.where({:id => params[:id]}).limit(1)
         if @product.present?
+            @item = {}
+            @item[:id] = @product[0][:id]
+            @item[:name] = @product[0][:name]
+            @item[:description] = @product[0][:description]
+            @item[:enable] = @product[0][:enable]
+            
+            @product_image = ProductImage.where({:product_id => params[:id]}).limit(1)
+            if @product_image.length != 0
+
+                @image = Image.find(@product_image[0]['image_id']) 
+                @item[:image] = @image.file.url
+                
+            else
+                @item[:image] = '-'
+            end
+
+            @category_product = CategoryProduct.where({:product_id => @item[:id]}).limit(1)
+            if @category_product.length != 0
+
+                @category = Category.find(@category_product[0][:category_id])
+                @item[:image] = @category['name']
+            else
+
+                @item[:image] = '-'
+            end
+
             render json:{
-                values:@product,
+                values:@item,
                 message: 'Success!'
             }, status:200
         else
